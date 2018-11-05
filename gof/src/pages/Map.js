e8efd9ea60cc2183b296752ee63ee4b736e7510b
sofielanduyt <43749961+sofielanduyt@users.noreply.github.com>
2018-11-05 17:06:02 +0100

    deze app werkt - gof

---
+++ gof/src/pages/Map.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// eslint-disable-next-line
+import { Link } from 'react-router-dom';
+
+
+class Map extends React.Component {
+
+    render() {
+      return(
+        <div><h1>This is the map</h1></div>
+      );
+    }
+}
+
+export default Map;
