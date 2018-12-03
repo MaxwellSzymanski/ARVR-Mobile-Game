097077a346a9d3923204435029accaad876e4b11
Unknown <sem@viroux.org>
2018-12-03 15:28:09 +0100

    commit

--- Server tests/signintest.js
+++ Server tests/signintest.js
@@ -1,24 +1,29 @@
 const axios = require('axios');
 const https = require('https');
+const numberOfUsers = 10;
 
 const url = 'https://35.241.198.186:80';
 const agent = new https.Agent({
     rejectUnauthorized: false
 });
 
-for(let i = 0; i < 60; i++) {
+for(let i = 0; i < numberOfUsers; i++) {
     const name = "username_" + i.toString();
     const email = name + "@testusers.com";
     const password = "password";
+    const long = 4.6951758 + 0.01 * Math.random();
+    const lat = 50.864949499969994 + 0.01 * Math.random();
     const data = {
         email : email,
         password : password,
         request : "signin",
         position :
             {
-                longitude : 4.6951758,
-                latitude : 50.864949499969994
+                longitude : long,
+                latitude : lat
             }
     };
     axios.post(url, JSON.stringify(data), { httpsAgent: agent });
 }
+
+console.log(numberOfUsers + " signin requests sent.");
