1dd7c6a4dc0b8febd518d53031524a633dfadbd9
Sofie Landuyt <sofie.landuyt@student.kuleuven.be>
2018-12-03 16:54:39 +0100

    loader

--- gof/src/pages/Map.js
+++ gof/src/pages/Map.js
@@ -2,8 +2,11 @@ import React from 'react';
 // eslint-disable-next-line
 import { Link } from 'react-router-dom';
 import Radar from './Radar.js';
+import LoadingScreen from 'react-loading-screen';
+import LoaderComponent from './LoaderComponent.js';
 
 const url = require('./serveradress.js');
+
 
 class Map extends React.Component {
 
@@ -11,9 +14,14 @@ class Map extends React.Component {
 
     render() {
       return(
-      <div><a href="www.google.nl">GO</a></div>
-      );
+        <div className="background">
+        <div className="loader"></div>
+        <meta http-equiv="refresh" content="1; url=https://nl.wikipedia.org/wiki/Aardappel"></meta>
+        </div>);
+
     }
 }
+
+
 
 export default Map;
