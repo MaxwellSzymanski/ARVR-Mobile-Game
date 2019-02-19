3bcf776c1bc2d4adcd784f71f5b8c06cc313a6f1
Patserbak <florentmeijer@hotmail.com>
2019-02-19 19:00:34 +0100

    jj
    jjj

--- frontend/src/pages/PlayerLayer.js
+++ frontend/src/pages/PlayerLayer.js
@@ -43,7 +43,7 @@ var enemyOffline = L.icon({
     shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [45, 45], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [-3,80] // point from which the popup should open relative to the iconAnchor
 });
 
 var pathMark = L.icon({
