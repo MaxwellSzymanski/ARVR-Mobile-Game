ca9c80e9b3b3815ef52bc06be68bfa6137c01896
Maxwell Szymanski <maxwell.szymanski@student.kuleuven.be>
2019-02-24 15:02:51 +0100

    uploadImage for iOS and Safari

--- frontend/src/pages/CameraComp.js
+++ frontend/src/pages/CameraComp.js
@@ -4,30 +4,34 @@ import 'react-html5-camera-photo/build/css/index.css';
 import { Redirect } from 'react-router';
 import { getFeatureVector } from '../facerecognition/FaceRecognition';
 import swal from '@sweetalert/with-react';
+import {isIOS, isSafari} from 'react-device-detect';
+import ImageUploader from 'react-images-upload';
 
 class CameraComp extends React.Component {
 
-  state = {
-    redirect : false }
-
-  setRedirect = () => {
-      this.setState({redirect: true})
+    state = {
+        redirect : false,
+        picture: null
     }
 
-  renderRedirect = () => {
-    if (this.state.redirect) {return <Redirect to="/imageConfirm" />}
-  }
+    setRedirect = () => {
+        this.setState({redirect: true})
+    }
 
-  async onTakePhoto (dataUri) {
-      // TODO: Disable Camera
-      var photoSrc = dataUri;
-      var photo = new Image;
-      photo.src = photoSrc;
-      console.log(photo);
+    renderRedirect = () => {
+        if (this.state.redirect) {return <Redirect to="/imageConfirm" />}
+    }
 
-      var fv = await getFeatureVector(photo);
+    async onTakePhoto (dataUri) {
+        // TODO: Disable Camera
+        var photoSrc = dataUri;
+        var photo = new Image;
+        photo.src = photoSrc;
+        console.log(photo);
 
-      if (fv === null) {
+        var fv = await getFeatureVector(photo);
+
+        if (fv === null) {
           console.log("No face detected!")
           swal({
               title: "No face detected",
@@ -35,21 +39,59 @@ class CameraComp extends React.Component {
               icon: "warning",
               button: "I'll try!",
           });
-      }
-      else {
+        }
+        else {
           localStorage.setItem("PhotoOfMe", dataUri);
           localStorage.setItem("fv", fv);
           this.setRedirect();
-      }
+        }
 
-  }
+    }
 
-  redirect() {
-    return <Redirect to="/view"/>;
-  }
+    redirect() {
+        return <Redirect to="/view"/>;
+    }
+
+    handleUpload(picture) {
+        //TODO: Handle image upload.
+        if (picture.toString() !== "") {
+            // Picture uploaded.
+            this.state.picture = picture;
+            // TODO: Make sure to redirect.
+        }
+        else {
+            // Error, either too big or
+            swal("Make sure the picture is jpg or png and smaller than 5MB.")
+        }
+    }
+
+    showInfo() {
+        swal("Why am I seeing this page?", "Since you are using an iOS device, the camera component is not" +
+        " compatible with the web app. That's why we are offering you a way to choose a photo from your camera roll.")
+    }
 
 
-  render () {
+    render () {
+      if (isSafari || isIOS) return (
+          <div>
+              <span style={{margin:'20px'}}></span>
+              <div className="polaroid" style={{width:'500px'}}>
+                  <ImageUploader
+                      withIcon={true}
+                      buttonText='Choose profile image'
+                      onChange={this.handleUpload}
+                      imgExtension={['.jpg', '.png']}
+                      maxFileSize={5242880}
+                      withLabel={false}
+                      fileSizeError={"Make sure it is smaller than 5MB."}
+                      fileTypeError={"Make sure it is a jpg or png image."}
+                      singleImage={true}
+                  />
+              </div>
+              <button className="smallButton info" style={{marginTop:'20px'}} onClick={this.showInfo}></button>
+          </div>
+    )
+
     return (
       <div className="background">
         {this.renderRedirect()}
@@ -63,7 +105,7 @@ class CameraComp extends React.Component {
         </div>
       </div>
     );
-  }
+    }
 }
 
 export default CameraComp;
