232bca453e64988bcddf6c78fbc98f94f945c6d9
Sem Viroux <sem.viroux@hotmail.com>
2019-03-27 21:12:27 +0100

    verified: false
    Ja Maxwell, ik weet dat gij het waart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,8 +31,8 @@ class App extends Component {
         this.state = {
             socket: io(url),
             loggedIn: false,
-            //verified: false,
-            verified: true
+            verified: false,
+            // verified: true
         };
 
     }
