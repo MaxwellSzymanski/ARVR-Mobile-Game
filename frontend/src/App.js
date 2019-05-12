72cd83546c64f53e3e62b8d81c253cfb97e84810
Sem Viroux <sem.viroux@hotmail.com>
2019-05-12 15:51:18 +0200

    verified: false

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,8 +32,8 @@ class App extends Component {
         this.state = {
             socket: io(url),
             loggedIn: false,
-            // verified: false,
-            verified: true,
+            verified: false,
+            // verified: true,
         };
     }
 
